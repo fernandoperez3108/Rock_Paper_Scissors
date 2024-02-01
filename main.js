e465bad5727f29387ba6622e20fa184d4f1284e2
fernandoperez3108 <fernandoperezfloresjr@gmail.com>
2024-02-01 15:42:41 -0400

    feat:finish game

--- main.js
+++ main.js
@@ -1,57 +1,56 @@
-let getComputerChoice = () => {
-    let query = Math.floor(Math.random() * 3);
-    switch (query) {
-      case 0:
-        return 'rock';
-        break;
-  
-      case 1:
-        return 'paper';
-        break;
-  
-      default:
-        return 'scissors';
-    }
-  }
-  
-  let getPlayerChoice = () => {
-    let choice = prompt('Give me your election please');
-    let toLower = choice.toLowerCase();
-    return toLower;
-  }
-  
-  let playRound = (playerSelection, computerSelection) => {
-    if ((playerSelection === 'rock' && computerSelection === 'scissors') || (playerSelection === 'paper' && computerSelection === 'rock') || (playerSelection === 'scissors' && computerSelection === 'paper')) {
-      return `You Win! ${playerSelection} beats ${computerSelection}`;
-    } else if (playerSelection === computerSelection) {
-      return 'It\'s a tie';
-    } else {
-      return `You Lose! ${computerSelection} beats ${playerSelection}`;
-    }
-  }
+let playerScore = 0
+let computerScore = 0
+const buttons = document.querySelectorAll('input')
 
-  let game = () => {
-    let computerPoints = 0;
-    let playerPoints = 0;
-    while(computerPoints < 5 && playerPoints < 5) {
-      playerSelection = getPlayerChoice();
-      computerSelection = getComputerChoice();
-      if (playRound(playerSelection, computerSelection) === `You Win! ${playerSelection} beats ${computerSelection}`) {
-        playerPoints++;
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}, you have ${playerPoints} and the computer have ${computerPoints}`);
-      } else if (playRound(playerSelection, computerSelection) === 'It\'s a tie') {
-        console.log('It\'s a tie');
-        continue;
-      } else {
-        computerPoints++;
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}, you have ${playerPoints} and the computer have ${computerPoints}`);
-      }
+function computerPlay() {
+    let choices = ['rock', 'paper', 'scissors']
+    return choices[Math.floor(Math.random() * choices.length)]
+}
+
+function disableButtons() {
+    buttons.forEach(elem => {
+        elem.disabled = true
+    })
+}
+
+function playRound(playerSelection) {
+    let computerSelection = computerPlay()
+    let result = ""
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+        
+        playerScore += 1
+        result = ('You win! ' + playerSelection + ' beats ' + computerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (playerScore == 5) {
+            result += '<br><br>You won the game! Reload the page to play again'
+            disableButtons()
+        }
     }
-    if (playerPoints > computerPoints) {
-      console.log('Congratulaion, you finally win');
-    } else {
-      console.log('Sorry, you finally lose, please try again');
+    else if (playerSelection == computerSelection) {
+        result = ('It\'s a tie. You both chose ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
     }
-  }
-  
-  game();
+    else {
+        computerScore += 1
+        result = ('You lose! ' + computerSelection + ' beats ' + playerSelection
+            + "<br><br>Player score: " + playerScore + "<br>Computer score: " + computerScore)
+
+        if (computerScore == 5) {
+            result += '<br><br>I won the game! Reload the page to play again'
+            disableButtons()
+        }
+    }
+
+    document.getElementById('result').innerHTML = result
+    return
+}
+
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+        playRound(button.value)
+    })
+})
